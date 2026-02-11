56886a023fa105b7a477f9dccde842097fbff77a
root <root@srv1322675.hstgr.cloud>
2026-02-11 10:18:40 +0000

    local CORS / OAuth updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -43,8 +43,6 @@ if (process.env.NODE_ENV !== 'production') {
 const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(helmet()); // Security headers
 
 // CORS configuration
 app.use(
@@ -56,13 +54,17 @@ app.use(
       "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization","x-session-token"],
     credentials: true
   })
 );
 
 app.options("*", cors());
-
+app.use(
+helmet(
+{
+    crossOriginResourcePolicy: false,
+  }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
